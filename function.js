0fa71633fd1f250c234ba85ad609fb9e3ae874ef
rahul <rahul.valasai@gmail.com>
2022-07-03 15:12:29 +0530

    function-using-add-sub-mul-div.

---
+++ function.js
@@ -0,0 +1,48 @@
+//addition
+function add()
+{
+ let a = 10;
+
+ let b = 5;
+
+ let add1 = a + b;
+
+ console.log(add1);
+}
+add()
+ 
+
+//subtrtaction
+let x = 10,y = 5;
+
+function subtrat(a,b)
+{
+  sub = x - y;
+
+console.log(sub);
+}
+subtrat()
+// //multiplication
+
+let e = 10,f = 5;
+function multi(x,y)
+{
+ let mul = e * f;
+
+ console.log("multiplication is:",mul);
+}
+multi()
+
+// // division
+
+let g = 10, h = 20;
+
+let div = 0;
+
+function divi()
+{
+  let div = g / h;
+  console.log("division is:",div);
+}
+
+divi()
